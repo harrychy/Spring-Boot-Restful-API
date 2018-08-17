56c1402279adedd8dbd980527cc5ab903d882b20
Harry Cao <1070170859@qq.com>
2018-08-17 12:52:17 +0800

    Delete

--- src/main/java/com/example/SpringBootRestfulAPI/TVSeriesController.java
+++ src/main/java/com/example/SpringBootRestfulAPI/TVSeriesController.java
@@ -6,6 +6,7 @@ import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
 @RestController//返回Json
@@ -49,6 +50,23 @@ public class TVSeriesController {
             log.trace("updateOne: "+ id);
         }
         return getTvSeriesDto(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Map<String,String> deleteOne(@PathVariable int id, HttpServletRequest request,
+                                        @RequestParam(value = "delete_reason",required = false)String deleteReason)throws Exception{
+        if(log.isTraceEnabled()){
+            log.trace("deleteOne" + id);
+        }
+        Map<String,String> result = new HashMap<>();
+        if(id == 101){
+            result.put("message","#101被" + request.getRemoteAddr() + "删除（原因" +deleteReason+"）");
+        }else if(id ==102){
+            throw new RuntimeException("#102不能删除");
+        }else {
+            throw new ResourceNotFoundException();
+        }
+        return result;
     }
 
     private TVSeriesDto getTvSeriesDto(@PathVariable int id) {
